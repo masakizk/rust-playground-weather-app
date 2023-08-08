317835b28f80a511606247ead1d9af22d2da35ef
zacker <robomasaki2@gmail.com>
2023-08-09 08:59:57 +0900

    feat: make sample request

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
+use std::collections::HashMap;
 use std::env;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 {
-        println!("Please provide an API key");
-        return;
+        return Err("No API key provided".into());
     }
 
     let api_key = &args[1];
     println!("Using Open Weather Map API key: {}", api_key);
+
+    let resp = reqwest::get("https://httpbin.org/ip")
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?;
+    println!("{:#?}", resp);
+
+    return Ok(());
 }
