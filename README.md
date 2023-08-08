648966b1ecde6ff41980460067c86191e96ea74c
zacker <robomasaki2@gmail.com>
2023-08-09 08:56:05 +0900

    feat: receive api key by command line arguments

---
+++ README.md
@@ -0,0 +1,10 @@
+# Weather App
+
+### 概要
+Open Weather Mapから天気を出力し、それをJSONファイルに出力する
+
+### 使い方
+
+```shell
+cargo run your_api_key
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("Please provide an API key");
+        return;
+    }
+
+    let api_key = &args[1];
+    println!("Using Open Weather Map API key: {}", api_key);
 }
